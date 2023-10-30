2466feb92391333cf16e37091d851891a777aca0
Marlène <marlene@MacBook-Pro-de-Marlene.local>
2023-10-30 13:35:50 +0100

    mariadb clean

--- srcs/requirements/mariadb/init_db.sh
+++ srcs/requirements/mariadb/init_db.sh
@@ -8,27 +8,30 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     # Start MariaDB in the background to perform initial operations
     mariadbd-safe --datadir='/var/lib/mysql' --no-watch &
 
-    # Wait for MariaDB to start
-    for i in {1..30}; do
-        if echo 'SELECT 1' | mariadb -u root &> /dev/null; then
-            break
-        fi
-        sleep 1
-    done
+    # # Wait for MariaDB to start
+    echo "waiting for marlon db to start..."
+    # for i in {1..30}; do
+    #     echo "trying..."
+    #     if echo 'SELECT 1' | mariadb -u root &> /dev/null; then
+    #         break
+    #     fi
+    #     sleep 1
+    # done
+    sleep 10
 
     # Secure initial MariaDB setup
-
-    mariadb -uroot -h localhost -e "DROP DATABASE test"
-    mariadb -uroot -h localhost -e "DROP USER ''@'"localhost"'"
-    mariadb -uroot -h localhost -e "DROP USER ''@'"$(hostname)"'"
-    mariadb -uroot -h localhost -e "CREATE DATABASE $MYSQL_DATABASE"
-    mariadb -uroot -h localhost -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
-    mariadb -uroot -h localhost -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* to '$MYSQL_USER'@'%'"
-    mariadb -uroot -h localhost -e "FLUSH PRIVILEGES"
-    mariadb -uroot -h localhost -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
-
+    echo "Starting setup..."
+    mariadb -u root -h localhost -e "DROP DATABASE test"
+    mariadb -u root -h localhost -e "DROP USER ''@'"localhost"'"
+    mariadb -u root -h localhost -e "DROP USER ''@'"$(hostname)"'"
+    mariadb -u root -h localhost -e "CREATE DATABASE $MYSQL_DATABASE"
+    mariadb -u root -h localhost -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
+    mariadb -u root -h localhost -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* to '$MYSQL_USER'@'%'"
+    mariadb -u root -h localhost -e "FLUSH PRIVILEGES"
+    mariadb -u root -h localhost -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
+    echo "setup done :D"
     # Shut down the temporary MariaDB instance
-    pkill maria
+    mysqladmin -u root --password=$MYSQL_ROOT_PASSWORD -h localhost shutdown
     sleep 5
 fi
 
--- srcs/requirements/wordpress/auto_config.sh
+++ srcs/requirements/wordpress/auto_config.sh
@@ -5,7 +5,7 @@ COUNTER=0
 # sleep 10000
 # Wait for MariaDB to be ready
 waitForMariaDB() {
-    until echo 'SELECT 1' | mysql -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" &> /dev/null || [ $COUNTER -eq $MAX_RETRIES ]; do
+    until echo 'SELECT 1' | mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p "$MYSQL_PASSWORD" &> /dev/null || [ $COUNTER -eq $MAX_RETRIES ]; do
         COUNTER=$((COUNTER+1))
         echo "Waiting for MariaDB to be ready... (Attempt: $COUNTER)"
         sleep 1
@@ -21,17 +21,26 @@ waitForMariaDB() {
 setupWordPress() {
     if ! $(wp core is-installed --allow-root); then
         wp core download --allow-root
-        wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="$MYSQL_HOST" --allow-root
+        echo "DOWNLOADED"
+        wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost=127.0.0.1 --allow-root
+        echo "CONFIG CREATED"
         wp core install --allow-root --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_email="$WP_ADMIN_EMAIL" --admin_password="$WP_ADMIN_PASSWORD"
+        echo "INSTALLED"
         wp user create "$WP_USER" "$WP_USER_EMAIL" --role=author --user_pass="$WP_USER_PASSWORD" --allow-root
+        echo "USER CREATED"
     fi
 }
 
 main() {
     # cd /var/www/html
-    waitForMariaDB
+    # waitForMariaDB
     setupWordPress
-    /usr/sbin/php-fpm8.2 -F -R
+    echo "going to run "
+    /usr/sbin/php-fpm7.3 -F -R
 }
 
 main
+
+
+trouver un moyen que cette image elle run sans rien
+depuis le terminal wp, faire la commande mysql pour se connecter 
